192d82e857944279afb67a9d21c755f1a6d4703b
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-03-30 20:24:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let text = document.getElementById('text').value;
+let delay = document.getElementById('delay').value;
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function displayMessage() {
+  let text = document.getElementById('text').value;
+  let delay = document.getElementById('delay').value;
+  
+  await sleep(delay);
+  
+  document.getElementById('output').innerText = text;
+}
+
+document.getElementById('btn').addEventListener('click', displayMessage);
